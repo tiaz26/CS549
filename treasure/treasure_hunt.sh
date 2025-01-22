abf4cf6e8403d27b07fdceb02b56c7894b7af4d9
tiaz26 <tiaz04@bu.edu>
2025-01-22 16:51:00 -0500

    treasure hunt exercise done

---
+++ treasure/treasure_hunt.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Your code here
+
+mkdir ml_treasure_hunt
+
+echo "The treasure is hidden in plain sight" > ml_treasure_hunt/clue_1.txt
+
+mkdir ml_treasure_hunt/secret_chamber
+
+echo "Look for a hidden file" > ml_treasure_hunt/secret_chamber/clue_2.txt
+
+echo "Congratulations! You've found the treasure!" > ml_treasure_hunt/.treasure_map.txt
